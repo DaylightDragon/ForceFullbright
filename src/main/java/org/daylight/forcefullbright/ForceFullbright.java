4ebdec7485d2cc716ca418cbcfe80722dbc50b1c
DaylightDragon <work.daylight@gmail.com>
2026-03-08 23:11:47 +0300

    Fixes

--- src/main/java/org/daylight/forcefullbright/ForceFullbright.java
+++ src/main/java/org/daylight/forcefullbright/ForceFullbright.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ForceFullbright implements ModInitializer {
-	public static final String MOD_ID = "chatutils";
+	public static final String MOD_ID = "forcefullbright";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
